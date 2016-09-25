724413294c65d6907a33413d8ac33cd7bbbec153
foskon <foskon@gmail.com>
2016-09-25 11:55:37 +0200

    First commit.

---
+++ OpenLibrary/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  OpenLibrary
+//
+//  Created by Carlos Manzanas on 25/09/16.
+//  Copyright (c) 2016 foskon. All rights reserved.
+//
+
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+
+}
